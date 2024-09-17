e76382ef5a4803b2f20dd3a696bc578856b212d6
SAMIUL2022 <sami35bd@gmail.com>
2024-09-17 20:27:43 +0600

    last commit

--- S_Max_Split.py
+++ S_Max_Split.py
@@ -1,29 +1,21 @@
 import pyautogui
 import time
 
-# Get the height of the pyramid
+
 n = int(input("Enter the height of the pyramid: "))
 
-# Inform the user to switch to the text editor
+
 print("You have 5 seconds to switch to your text editor...")
 time.sleep(5)
 
 def type_pyramid(n):
     for i in range(n):
  
-       
-
-        
-      
         pyautogui.typewrite('#' * (i + 1),interval=0.25)
         
-        
+
         pyautogui.press('enter')
 
 
 type_pyramid(n)
-#
-##
-###
-####
-#####
+
